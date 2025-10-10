f4b4bd64c6ba7ed2ba5e076ca3a69424506ca57f
HiwalePriyanka94 <hiwalepriyanka894@gmail.com>
2025-10-10 21:55:31 +0530

    terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+
+variable "aws_region" {
+  description = "AWS region to deploy"
+  default     = "ap-south-1"
+}
+
+variable "cluster_name" {
+  description = "EKS Cluster name"
+  default     = "boardgame"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
